25b1c2659b068ebffa9d8af2f5273a382ed7602e
elgas <elgasmaci@gmail.com>
2022-01-28 00:49:21 +0000

    Create the Flask App

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+@app.route("/")
+#@app.route("/get_workout")
+#def get_workout():
+#    workouts = mongo.db.workouts.find()
+#    return render_template("workouts.html", workouts = workouts)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
