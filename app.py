03fe3aa44b71aaabfb35df3a55dc56e59183a132
elgas <elgasmaci@gmail.com>
2022-01-28 01:20:15 +0000

    Connect Flask to MangoDB

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 @app.route("/")
-#@app.route("/get_workout")
-#def get_workout():
-#    workouts = mongo.db.workouts.find()
-#    return render_template("workouts.html", workouts = workouts)
+@app.route("/get_workouts")
+def get_workouts():
+    workouts = mongo.db.workouts.find()
+    return render_template("workouts.html", workouts=workouts)
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+    app.run(host=os.environ.get("IP"), 
+             port=int(os.environ.get("PORT")),
+             debug=True)
